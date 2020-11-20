31c3ffe53e729e5587ac7cd903131c66b640d668
sstraybird <sstraybird@163.com>
2020-11-20 14:31:39 +0800

    添加项目文件。

---
+++ Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectDataProviderDemo
+{
+    class Person : INotifyPropertyChanged
+    {
+        public Person(string Name)
+        {
+            this.Name = Name;
+        }
+        public string Name { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
