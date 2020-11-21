992bfc6a264211948d297711be9b10743f686179
sstraybird <sstraybird@163.com>
2020-11-21 23:14:19 +0800

    参考官网代码修改

--- Person.cs
+++ Person.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,12 +10,35 @@ namespace ObjectDataProviderDemo
 {
     class Person : INotifyPropertyChanged
     {
-        public Person(string Name)
-        {
-            this.Name = Name;
-        }
-        public string Name { get; set; }
-
+        private string name;
+        // Declare the event
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public Person()
+        {
+        }
+
+        public Person(string value)
+        {
+            this.name = value;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged();
+            }
+        }
+
+        // Create the OnPropertyChanged method to raise the event
+        // The calling member's name will be used as the parameter.
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
